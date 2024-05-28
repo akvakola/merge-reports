3153ce93b9bdb4452d5c40ab9a7fb4e9d8af0a88
ndvik <viktor.novacki@endava.com>
2024-05-28 15:16:41 +0200

    blob

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,16 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["html", { outputFolder: "playwright-report/tests", project: "tests" }],
-    [
-      "html",
-      {
-        outputFolder: "playwright-report/tests-examples",
-        project: "tests-examples",
-      },
-    ],
-  ],
+  reporter: process.env.CI ? [["blob"], ["list"]] : [["blob"], ["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
